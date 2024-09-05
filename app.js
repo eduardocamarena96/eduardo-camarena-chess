0aaf1b7f5530201dda436639f8fe74498aa1714d
eduardocamarena96 <eduardocamarenabvt2024@gmail.com>
2024-09-05 05:23:13 -0700

    Final Draft Completed

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const gameboard = document.getElementById('gameboard');
 const playerDisplay = document.getElementById('player');
 const infoDisplay = document.getElementById('info-display');
 
-// 8 by 8 board
+// Set width to 8 because this will be an 8 by 8 board
 const width = 8;
-let playerGo = 'black';
-playerDisplay.textContent = 'black';
 
-// Setting up board with pieces by utilizing an array of 64 items
+let playerTurn = 'black';
+playerDisplay.textContent = 'black\'s';
+
+// Setting up board with game pieces by utilizing an array of 64 items
+// Since board is 8 rows and 8 columns, it will make up 64 squares or positions
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
@@ -19,23 +21,27 @@ const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook
 ]
 
-
-// display board in browser
+// Display board in browser
 function createBoard() {
+
     startPieces.forEach((startPiece, index) => {
+
+        // Create new div that has the square class and a child element of a chess piece if it's not an empty square
         const square = document.createElement('div');
         square.classList.add('square');
         square.innerHTML = startPiece;
 
-        // If square has a piece or first child element then add draggable class to piece element
-        // Optional chaining operator: If first child is null or undefined
-        // then setAttribute will not be called
+        // If a square has a chess piece as a child element then add the draggable class to the chess piece element
+        // Optional chaining operator: If first child is null or undefined then setAttribute will not be called
         square.firstChild?.setAttribute('draggable', true);
 
+        // Set the value of square-id to be the current index in the array (0-63)
         square.setAttribute('square-id', index);
 
-        // Add background color to squares on board
+        // Add background color to squares on board with alternating colors
         const row = Math.floor((63 - index) / 8) + 1;
+        // If row number is even (8, 6, 4, 2)
+        // Else row number is odd (7, 5, 3, 1)
         if (row % 2 === 0) {
             square.classList.add(index % 2 === 0 ? 'beige': 'brown');
         } else {
@@ -45,26 +51,35 @@ function createBoard() {
 
         // First 16 squares on the board
         if (index <= 15) {
-            // targets the svg inside piece div (square -> piece -> svg)
+            // targets the svg inside the chess piece div (square -> piece -> svg) and adds the black color class to it
             square.firstChild.firstChild.classList.add('black');
         }
 
+        // Last 16 squares on the board
         if (index >= 48) {
+            // targets the svg inside the chess piece div (square -> piece -> svg) and adds the white color class to it
             square.firstChild.firstChild.classList.add('white');
         }
 
-        gameboard.append(square);
-
+        // Add individual square to the gameboard div as a child element
+        gameboard.append(square); 
     })
 }
 
-
 createBoard();
 
+// Grab all squares on the gameboard 
 const allSquares = document.querySelectorAll('#gameboard .square');
+
+// Add event listeners for dragging actions to all squares on the gameboard
 allSquares.forEach(square => {
+    // The dragstart event is called once when the user starts to drag an element.
     square.addEventListener('dragstart', dragStart);
+
+    // The dragover event is called repeatedly while an element is being dragged.
     square.addEventListener('dragover', dragOver);
+
+    // The drop event is called once when an element is dropped on a valid drop target.
     square.addEventListener('drop', dragDrop);
 })
 
@@ -72,65 +87,100 @@ let startPositionId;
 let draggedElement;
 
 function dragStart(event) {
+    // event.target is the chess piece that was clicked on
+    // event.target.parentNode is the square where the chess piece is located
+    
+    // Get the position of the square where the clicked chess piece is located.
     startPositionId = event.target.parentNode.getAttribute('square-id');
+    // Get the chess piece currently being dragged
     draggedElement = event.target;
 }
 
 function dragOver(event) {
+    // By default, the dragover event does not allow dropping.
     event.preventDefault();
 }
 
 function dragDrop(event) {
+    // This is used to stop the event from bubbling up the DOM tree. This means that the event will not be passed to any of the parent elements
     event.stopPropagation();
 
-    const correctGo = draggedElement.firstChild.classList.contains(playerGo);
+    // draggedElement is the current chess piece being dragged
+    // event.target will either be the square where the dragged chess piece will be dropped on or the chess piece where the dragged chess piece will be dropped on
+
+    // Set correctTurn to true if the dragged chess piece has the correct color of the current player
+    const correctTurn = draggedElement.firstChild.classList.contains(playerTurn);
+    // Set taken to true if the square contains a chess piece
     const taken = event.target.classList.contains('piece');
 
+    // Set valid to true if the current chess piece being dragged can make that move legally
     const valid = checkIfValid(event.target);
 
-    const opponentGo = playerGo === 'white' ? 'black' : 'white';
-    const takenByOpponent = event.target.firstChild?.classList.contains(opponentGo); 
+    // Set opponentTurn to have the opposite color of the current player
+    const opponentTurn = playerTurn === 'white' ? 'black' : 'white';
 
-    if (correctGo) {
+    // Set takenByOpponent to true if the square contains a chess piece that belongs to the opponent's color
+    const takenByOpponent = event.target.firstChild?.classList.contains(opponentTurn); 
+
+    // If correct player's turn
+    if (correctTurn) {
+        // If the square is taken by a chess piece from the opponent and the dragged chess piece is making a valid move
         if (takenByOpponent && valid) {
+            // Add dragged chess piece to be a child of the target square
             event.target.parentNode.append(draggedElement);
+            // Remove opponent's chess piece from target square
             event.target.remove();
             checkForWin();
-            changePlayer();
+            changePlayerTurn();
             return;
         }
 
+        // If square is taken by a chess piece and it's not from the opponent
         if (taken && !takenByOpponent) {
             infoDisplay.textContent = 'you cannot go here!';
             setTimeout(() => infoDisplay.textContent = '', 2000);
             return;
         }
 
+        // If square is not taken by any chess piece and the dragged chess piece is making a valid move
         if (valid) {
+            // Add dragged chess piece to be a child of the target square
             event.target.append(draggedElement);
             checkForWin();
-            changePlayer();
+            changePlayerTurn();
             return;
         }
+    } else {
+        infoDisplay.textContent = 'Not your turn...';
+        setTimeout(() => infoDisplay.textContent = '', 1000);
     }
 
 }
 
 function checkIfValid(target) {
+    // target is either an empty square or a chess piece
+
+    // Set targetId to be the position of the square within the gameboard
     const targetId = Number(target.getAttribute('square-id')) || Number(target.parentNode.getAttribute('square-id'));
+    // Set startId to be the position of the square the chess piece being dragged was initially located in
     const startId = Number(startPositionId);
+    // Set piece to be the current chess piece being dragged
     const piece = draggedElement.id;
 
     switch(piece) {
         case 'pawn': 
             const starterRow = [8, 9, 10, 11, 12, 13, 14, 15];
             if (
+                // Allows pawn to move 2 squares forward if in starting row
                 starterRow.includes(startId) && startId + width * 2 === targetId
                 ||
-                startId + width === targetId
+                // Allows pawn to move 1 square forward
+                startId + width === targetId && !document.querySelector(`[square-id="${startId + width}"]`).firstChild
                 ||
+                // Allows pawn to move diagonal left if an opponent's chess piece is there
                 startId + width - 1 === targetId && document.querySelector(`[square-id="${startId + width - 1}"]`).firstChild
                 || 
+                // Allows pawn to move diagonal right if an opponent's chess piece is there
                 startId + width + 1 === targetId && document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild
             ) {
                 return true;
@@ -159,7 +209,7 @@ function checkIfValid(target) {
             break;
         case 'bishop': 
             if (
-                // Right Forward Diagonal Direction
+                // Forward Right Diagonal Direction
                 startId + width + 1 === targetId 
                 || 
                 startId + width * 2 + 2 && !document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild
@@ -176,7 +226,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Left Forward Diagonal Direction
+                // Backwards Left Diagonal Direction
                 startId - width - 1 === targetId 
                 || 
                 startId - width * 2 - 2 && !document.querySelector(`[square-id="${startId - width - 1}"]`).firstChild
@@ -193,7 +243,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Backwards Left Diagonal Direction
+                // Backwards Right Diagonal Direction
                 startId - width + 1 === targetId 
                 || 
                 startId - width * 2 + 2 === targetId && !document.querySelector(`[square-id="${startId - width + 1}"]`).firstChild
@@ -210,7 +260,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Backwards Right Diagonal Direction
+                // Forward Left Diagonal Direction
                 startId + width - 1 === targetId 
                 || 
                 startId + width * 2 - 2 === targetId && !document.querySelector(`[square-id="${startId + width - 1}"]`).firstChild
@@ -230,7 +280,7 @@ function checkIfValid(target) {
             break;
         case 'rook': 
             if (
-                // Checking below piece
+                // Checking above rook
                 startId + width === targetId 
                 ||
                 startId + width * 2 === targetId && !document.querySelector(`[square-id="${startId + width}"]`).firstChild
@@ -247,7 +297,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Checking above piece
+                // Checking below rook
                 startId - width === targetId 
                 ||
                 startId - width * 2 === targetId && !document.querySelector(`[square-id="${startId - width}"]`).firstChild
@@ -264,7 +314,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Check right of piece
+                // Check right of rook
                 startId + 1 === targetId 
                 ||
                 startId + 2 === targetId && !document.querySelector(`[square-id="${startId + 1}"]`).firstChild
@@ -281,7 +331,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Check left of piece
+                // Check left of rook
                 startId - 1 === targetId 
                 ||
                 startId - 2 === targetId && !document.querySelector(`[square-id="${startId - 1}"]`).firstChild
@@ -303,7 +353,7 @@ function checkIfValid(target) {
         case 'queen':
             if (
                     
-                // Right Forward Diagonal Direction
+                // Forward Right Diagonal Direction
                 startId + width + 1 === targetId 
                 || 
                 startId + width * 2 + 2 === targetId && !document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild
@@ -320,7 +370,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Left Forward Diagonal Direction
+                // Backwards Left Diagonal Direction
                 startId - width - 1 === targetId 
                 || 
                 startId - width * 2 - 2 === targetId && !document.querySelector(`[square-id="${startId - width - 1}"]`).firstChild
@@ -337,7 +387,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Backwards Diagonal Direction
+                // Backwards Right Diagonal Direction
                 startId - width + 1 === targetId 
                 || 
                 startId - width * 2 + 2 === targetId && !document.querySelector(`[square-id="${startId - width + 1}"]`).firstChild
@@ -354,7 +404,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Backwards Diagonal Direction
+                // Forward Left Diagonal Direction
                 startId + width - 1 === targetId 
                 || 
                 startId + width * 2 - 2 === targetId && !document.querySelector(`[square-id="${startId + width - 1}"]`).firstChild
@@ -371,7 +421,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Checking below piece
+                // Checking above queen
                 startId + width === targetId 
                 ||
                 startId + width * 2 === targetId && !document.querySelector(`[square-id="${startId + width}"]`).firstChild
@@ -388,7 +438,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Checking above piece
+                // Checking below queen
                 startId - width === targetId 
                 ||
                 startId - width * 2 === targetId && !document.querySelector(`[square-id="${startId - width}"]`).firstChild
@@ -405,7 +455,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Check right of piece
+                // Check right of queen
                 startId + 1 === targetId 
                 ||
                 startId + 2 === targetId && !document.querySelector(`[square-id="${startId + 1}"]`).firstChild
@@ -422,7 +472,7 @@ function checkIfValid(target) {
 
                 ||
 
-                // Check left of piece
+                // Check left of queen
                 startId - 1 === targetId 
                 ||
                 startId - 2 === targetId && !document.querySelector(`[square-id="${startId - 1}"]`).firstChild
@@ -444,13 +494,21 @@ function checkIfValid(target) {
             break;
         case 'king': 
             if (
+                // Move right 1 square
                 startId + 1 === targetId ||
+                // Move left 1 square
                 startId - 1 === targetId ||
+                // Move forward 1 square
                 startId + width === targetId || 
+                // Move backward 1 square
                 startId - width === targetId ||
+                // Move forward diagonal left 1 square
                 startId + width - 1 === targetId ||
+                // Move forward diagonal right 1 square
                 startId + width + 1 === targetId ||
+                // Move backward diagonal left 1 square
                 startId - width - 1 === targetId ||
+                // Move backward diagonal right 1 square
                 startId - width + 1 === targetId
             ) {
                 return true;
@@ -459,36 +517,43 @@ function checkIfValid(target) {
     }
 }
 
-function changePlayer() {
-    if (playerGo === 'black') {
+function changePlayerTurn() {
+    if (playerTurn === 'black') {
         reverseIds();
-        playerGo = 'white';
-        playerDisplay.textContent = 'white';
+        playerTurn = 'white';
+        playerDisplay.textContent = 'white\'s';
     } else {
         revertIds();
-        playerGo = 'black';
-        playerDisplay.textContent = 'black';
+        playerTurn = 'black';
+        playerDisplay.textContent = 'black\'s';
     }
 }
 
+// Flip the position numbers of the squares so that the 1st position goes from position 0 to position 63 and so on...
 function reverseIds() {
     const allSquares = document.querySelectorAll('.square');
     allSquares.forEach((square, index) => square.setAttribute('square-id', (width * width - 1) - index));
 }
 
+// Flip the position numbers of the squares so that the 1st position goes from position 63 to position 0 and so on...
 function revertIds() {
     const allSquares = document.querySelectorAll('.square');
     allSquares.forEach((square, index) => square.setAttribute('square-id', index));
 }
 
+
 function checkForWin() {
+    // Grab both king elements and put them inside an array
     const kings = Array.from(document.querySelectorAll('#king'));
+
+    // If the array is missing a white king, then black player wins
     if (!kings.some(king => king.firstChild.classList.contains('white'))) {
         infoDisplay.innerHTML = "Black player wins!";
         const allSquares = document.querySelectorAll('.square');
         allSquares.forEach(square => square.firstChild?.setAttribute('draggable', false))
     }
 
+    // If the array is missing a black king, then white player wins
     if (!kings.some(king => king.firstChild.classList.contains('black'))) {
         infoDisplay.innerHTML = "White player wins!";
         const allSquares = document.querySelectorAll('.square');
