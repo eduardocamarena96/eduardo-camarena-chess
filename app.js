0fb54c0de06a7feaf3dba040f6a9901b2759cfcf
eduardocamarena96 <eduardocamarenabvt2024@gmail.com>
2024-09-05 19:41:45 -0700

    Removed bug allowing pawn at starting position to take over opponent's piece if they were 2 spaces in front

--- app.js
+++ app.js
@@ -167,13 +167,12 @@ function checkIfValid(target) {
     // Set piece to be the current chess piece being dragged
     const piece = draggedElement.id;
 
-    console.log('startid: ', startId);
     switch(piece) {
         case 'pawn': 
             const starterRow = [8, 9, 10, 11, 12, 13, 14, 15];
             if (
                 // Allows pawn to move 2 squares forward if in starting row
-                starterRow.includes(startId) && startId + width * 2 === targetId
+                starterRow.includes(startId) && startId + width * 2 === targetId && !document.querySelector(`[square-id="${startId + width * 2}"]`).firstChild
                 ||
                 // Allows pawn to move 1 square forward
                 startId + width === targetId && !document.querySelector(`[square-id="${startId + width}"]`).firstChild
